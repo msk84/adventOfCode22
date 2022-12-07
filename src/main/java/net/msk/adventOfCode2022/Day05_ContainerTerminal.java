9357b7b3346bd1ba52c7a996df9a7f081cef1e2a
msk84 <florian@steinle.it>
2022-12-07 22:33:54 +0100

    Solution day 5, task 2

--- src/main/java/net/msk/adventOfCode2022/Day05_ContainerTerminal.java
+++ src/main/java/net/msk/adventOfCode2022/Day05_ContainerTerminal.java
@@ -1,8 +1,6 @@
 package net.msk.adventOfCode2022;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class Day05_ContainerTerminal {
 
@@ -12,13 +10,25 @@ public class Day05_ContainerTerminal {
         this.containerTerminal.add(stack);
     }
 
-    public void moveContainers(final int amount, final int from, final int to) {
+    public void moveContainersOneByOne(final int amount, final int from, final int to) {
         for(int i = 0; i < amount; i++) {
             final Stack<Character> fromStack = this.containerTerminal.get(from - 1);
             final Stack<Character> toStack = this.containerTerminal.get(to - 1);
             final Character container = fromStack.pop();
             toStack.push(container);
         }
+    }
+
+    public void moveContainersMultiple(final int amount, final int from, final int to) {
+        final List<Character> movingContainers = new ArrayList<>();
+        final Stack<Character> fromStack = this.containerTerminal.get(from - 1);
+        final Stack<Character> toStack = this.containerTerminal.get(to - 1);
+
+        for(int i = 0; i < amount; i++) {
+            movingContainers.add(fromStack.pop());
+        }
+        Collections.reverse(movingContainers);
+        movingContainers.forEach(toStack::push);
     }
 
     public List<Character> getTopLine() {
--- src/test/java/net/msk/adventOfCode2022/Day05Test.java
+++ src/test/java/net/msk/adventOfCode2022/Day05Test.java
@@ -27,7 +27,7 @@ public class Day05Test extends BaseTest {
 
         lines.forEach(l -> {
             final String[] splitString = l.split(" ");
-            terminal.moveContainers(Integer.parseInt(splitString[1]), Integer.parseInt(splitString[3]), Integer.parseInt(splitString[5]));
+            terminal.moveContainersOneByOne(Integer.parseInt(splitString[1]), Integer.parseInt(splitString[3]), Integer.parseInt(splitString[5]));
         });
 
         final List<Character> topLine = terminal.getTopLine();
@@ -53,7 +53,7 @@ public class Day05Test extends BaseTest {
 
         lines.forEach(l -> {
             final String[] splitString = l.split(" ");
-            terminal.moveContainers(Integer.parseInt(splitString[1]), Integer.parseInt(splitString[3]), Integer.parseInt(splitString[5]));
+            terminal.moveContainersOneByOne(Integer.parseInt(splitString[1]), Integer.parseInt(splitString[3]), Integer.parseInt(splitString[5]));
         });
 
         final List<Character> topLine = terminal.getTopLine();
@@ -72,5 +72,62 @@ public class Day05Test extends BaseTest {
         final Stack<Character> stack = new Stack<>();
         charList.forEach(stack::push);
         return stack;
+    }
+
+
+    @Test
+    public void task2SimpleTest() {
+        final List<String> lines = new ArrayList<>();
+        lines.add("move 1 from 2 to 1");
+        lines.add("move 3 from 1 to 3");
+        lines.add("move 2 from 2 to 1");
+        lines.add("move 1 from 1 to 2");
+
+        final Day05_ContainerTerminal terminal = new Day05_ContainerTerminal();
+        terminal.addStack(this.createStack(List.of('Z', 'N')));
+        terminal.addStack(this.createStack(List.of('M', 'C', 'D')));
+        terminal.addStack(this.createStack(List.of('P')));
+
+        lines.forEach(l -> {
+            final String[] splitString = l.split(" ");
+            terminal.moveContainersMultiple(Integer.parseInt(splitString[1]), Integer.parseInt(splitString[3]), Integer.parseInt(splitString[5]));
+        });
+
+        final List<Character> topLine = terminal.getTopLine();
+        assertEquals('M', topLine.get(0));
+        assertEquals('C', topLine.get(1));
+        assertEquals('D', topLine.get(2));
+    }
+
+    @Test
+    public void task2Test() throws IOException, URISyntaxException {
+        final List<String> lines = this.getDataFromFileAsList("day05_data");
+
+        final Day05_ContainerTerminal terminal = new Day05_ContainerTerminal();
+        terminal.addStack(this.createStack(List.of('B', 'L', 'D', 'T', 'W', 'C', 'F', 'M')));
+        terminal.addStack(this.createStack(List.of('N', 'B', 'L')));
+        terminal.addStack(this.createStack(List.of('J', 'C', 'H', 'T', 'L', 'V')));
+        terminal.addStack(this.createStack(List.of('S', 'P', 'J', 'W')));
+        terminal.addStack(this.createStack(List.of('Z', 'S', 'C', 'F', 'T', 'L', 'R')));
+        terminal.addStack(this.createStack(List.of('W', 'D', 'G', 'B', 'H', 'N', 'Z')));
+        terminal.addStack(this.createStack(List.of('F', 'M', 'S', 'P', 'V', 'G', 'C', 'N')));
+        terminal.addStack(this.createStack(List.of('W', 'Q', 'R', 'J', 'F', 'V', 'C', 'Z')));
+        terminal.addStack(this.createStack(List.of('R', 'P', 'M', 'L', 'H')));
+
+        lines.forEach(l -> {
+            final String[] splitString = l.split(" ");
+            terminal.moveContainersMultiple(Integer.parseInt(splitString[1]), Integer.parseInt(splitString[3]), Integer.parseInt(splitString[5]));
+        });
+
+        final List<Character> topLine = terminal.getTopLine();
+        assertEquals('T', topLine.get(0));
+        assertEquals('Z', topLine.get(1));
+        assertEquals('L', topLine.get(2));
+        assertEquals('T', topLine.get(3));
+        assertEquals('L', topLine.get(4));
+        assertEquals('W', topLine.get(5));
+        assertEquals('R', topLine.get(6));
+        assertEquals('N', topLine.get(7));
+        assertEquals('F', topLine.get(8));
     }
 }
