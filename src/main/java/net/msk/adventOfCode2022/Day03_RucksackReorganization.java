e996e927a597c962ec7c5d4821da90a3b12b3eae
steinle <steinle@transporeon.com>
2022-12-07 17:45:44 +0100

    Simple solution day 3, task 2

--- src/main/java/net/msk/adventOfCode2022/Day03_RucksackReorganization.java
+++ src/main/java/net/msk/adventOfCode2022/Day03_RucksackReorganization.java
@@ -14,10 +14,19 @@ public class Day03_RucksackReorganization {
         }
     }
 
-    public static Character getDuplicateItem(final String items) {
+    public static Character findCommonItem(final String items) {
         final Set<Character> set1 = items.substring(0, (items.length()/2)).chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
         final Set<Character> set2 = items.substring((items.length()/2)).chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
         set1.retainAll(set2);
+        return set1.iterator().next();
+    }
+
+    public static Character findCommonItem(final String item1, final String item2, final String item3) {
+        final Set<Character> set1 = item1.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
+        final Set<Character> set2 = item2.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
+        final Set<Character> set3 = item3.chars().mapToObj(c -> (char)c).collect(Collectors.toSet());
+        set1.retainAll(set2);
+        set1.retainAll(set3);
         return set1.iterator().next();
     }
 }
--- src/test/java/net/msk/adventOfCode2022/Day03Test.java
+++ src/test/java/net/msk/adventOfCode2022/Day03Test.java
@@ -13,9 +13,26 @@ public class Day03Test extends BaseTest {
     public void task1Test() throws IOException, URISyntaxException {
         final List<String> lines = this.getDataFromFileAsList("day03_data");
         final int result = lines.stream()
-                .mapToInt(l -> Day03_RucksackReorganization.getItemPriority(Day03_RucksackReorganization.getDuplicateItem(l)))
+                .mapToInt(l -> Day03_RucksackReorganization.getItemPriority(Day03_RucksackReorganization.findCommonItem(l)))
                 .sum();
 
         assertEquals(7795, result);
+    }
+
+    @Test
+    public void task2Test() throws IOException, URISyntaxException {
+        final List<String> lines = this.getDataFromFileAsList("day03_data");
+        int sum = 0;
+
+        for(int i = 0; i < lines.size(); i=i+3) {
+            final String line1 = lines.get(i);
+            final String line2 = lines.get(i+1);
+            final String line3 = lines.get(i+2);
+
+            Character commonChar = Day03_RucksackReorganization.findCommonItem(line1, line2, line3);
+            sum += Day03_RucksackReorganization.getItemPriority(commonChar);
+        }
+
+        assertEquals(2703, sum);
     }
 }
